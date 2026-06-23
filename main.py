5834b4b6a14bbbec045cbe41ba0540cb3a500b84
VicIonesc <victorionescu21@gmail.com>
2026-06-22 23:56:12 -0400

    alpha 0.13 added second round to main

--- main.py
+++ main.py
@@ -69,3 +69,60 @@ data.weights = new_weights
 
 print("updated data weights: ", data.weights)
 print()
+
+print("BEST STUMP ROUND 2: ")
+
+best_stump = None
+best_error = float("inf")
+best_predictions = None
+
+for threshold in thresholds:
+    stump = Stump(threshold = threshold)
+    predictions = stump.predict_all(data.x)
+
+    error = 0
+
+    for i in range(len(data.y)):
+        if predictions[i] != data.y[i]:
+            error += data.weights[i]
+
+    if error < best_error:
+        best_error = error
+        best_stump = stump
+        best_predictions = predictions
+
+    print ("threshold:", threshold)
+    print ("predictions", predictions)
+    print ("error:", error)
+    print ()
+
+print ("BEST STUMP:")
+print ("threshold:", best_stump.threshold)
+print ("error:", best_error)
+print ("predictions:", best_predictions)
+
+
+alpha = 0.5 * math.log((1 - best_error) / best_error)
+
+print ("alpha:", alpha)
+
+new_weights = []
+
+for i in range (len(data.y)):
+    updated_weight = data.weights[i] * math.exp(-alpha * data.y[i] * best_predictions[i])
+    new_weights.append(updated_weight)
+
+print("new weights before normalization:", new_weights)
+
+total = sum(new_weights)
+
+for i in range(len(new_weights)):
+    new_weights[i] = new_weights[i] / total
+
+print("new weights after normalization:", new_weights)
+print("sum:", sum(new_weights))
+
+data.weights = new_weights
+
+print("updated data weights: ", data.weights)
+print()
